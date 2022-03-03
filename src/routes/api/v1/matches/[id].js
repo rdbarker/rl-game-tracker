5a21fc079db1b7799015fe40a77af68cc891d837
rdbarker <ryan.dean.barker@gmail.com>
2022-03-03 18:11:48 -0500

    check single match end point

---
+++ src/routes/api/v1/matches/[id].js
@@ -0,0 +1,7 @@
+import * as api from '$lib/api.js';
+
+export async function get({ request, params }) {
+	const headers = api.extractHeaders(request);
+	const route = `${api.BC_BASE_URL}replays/${params.id}`;
+	return await api.fetchResource(route, headers);
+}
