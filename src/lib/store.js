a81874fb50eccd465b3fb3cf732abf8631724b5c
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 22:29:29 -0500

    match que doesn't start immediately

--- src/lib/store.js
+++ src/lib/store.js
@@ -4,4 +4,3 @@ import { MatchesQueue } from './service/matchesQueue.js';
 export const apiKey = writable('');
 export const isKeyVerified = writable(false);
 export const matchQue = MatchesQueue(30);
-matchQue.start();
