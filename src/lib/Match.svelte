09f1ce2b50ee9948050c9f3ed789a63522ee560e
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 22:28:51 -0500

    match card init

---
+++ src/lib/Match.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { createMatchStats } from '$lib/model/match';
+	import { matchQue } from '$lib/store';
+	import TimeHeader from './match/TimeHeader.svelte';
+
+	export let matchId;
+	let matchStats;
+	let loading = true;
+
+	matchQue.fetchMatch((value) => {
+		matchStats = createMatchStats(value);
+		loading = false;
+	}, matchId);
+</script>
+
+{#if matchStats}
+	<article><TimeHeader timeString={matchStats.date} /></article>
+{/if}
