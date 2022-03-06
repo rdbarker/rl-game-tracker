40776c709c79db1be27a1c1e69810fa58399fd9c
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 22:28:59 -0500

    match list init

---
+++ src/lib/MatchList.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { matchQue } from '$lib/store';
+	import Match from '$lib/Match.svelte';
+	import { createMatches } from '$lib/model/matches';
+
+	let matches = new Map();
+	matchQue.fetchMatches((value) => {
+		matches = createMatches(value, matches);
+	});
+</script>
+
+<div>
+	{#each [...matches] as [matchId, matchBasicStats] (matchId)}
+		<Match {matchId} />
+	{/each}
+</div>
