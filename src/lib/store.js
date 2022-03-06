6eb0c0e4cc6939c3388ebaae4347b7c088b39e7b
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 21:07:04 -0500

    renamed a few things

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
-import { MatchQueue } from './service/matchesQueue.js';
+import { MatchesQueue } from './service/matchesQueue.js';
 
 export const apiKey = writable('');
 export const isKeyVerified = writable(false);
-export const matchQue = MatchQueue(30);
+export const matchQue = MatchesQueue(30);
 matchQue.start();
