dfdb1e2708c27f676756ea4acb59532eb6c208fd
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 22:44:01 -0500

    temp stats for team table

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,19 @@
-import { writable } from 'svelte/store';
+import { writable, readable } from 'svelte/store';
 import { MatchesQueue } from './service/matchesQueue.js';
 
 export const apiKey = writable('');
 export const isKeyVerified = writable(false);
 export const matchQue = MatchesQueue(30);
+
+const PLAYER_STATS = new Map([
+	['Score', 'score'],
+	['Goals', 'goals'],
+	['Shots', 'shots'],
+	['Shot%', 'shotPercentage'],
+	['Assists', 'assists'],
+	['Saves', 'saves'],
+	['Demos', 'demos']
+]);
+
+export const totalAvailblePlayerStats = readable(PLAYER_STATS);
+export const activeStats = writable(Array.from(PLAYER_STATS.keys()));
