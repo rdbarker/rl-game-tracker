8ed7ad063e9ddb176083a712f2ef44f489331348
rdbarker <ryan.dean.barker@gmail.com>
2022-03-05 22:30:13 -0500

    format time header init

---
+++ src/lib/match/TimeHeader.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let timeString;
+	let date = new Date(timeString);
+	const dateFormattingOptions = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric'
+	};
+	const dateFormatted = date.toLocaleDateString(undefined, dateFormattingOptions);
+	const timeFormatted = date.toLocaleTimeString();
+</script>
+
+{timeFormatted}
+{dateFormatted}
