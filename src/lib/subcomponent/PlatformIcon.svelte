629d74a5c258aa4baa636c33e5ffa25f020e21de
rdbarker <ryan.dean.barker@gmail.com>
2022-03-16 23:22:07 -0400

    icon component for platform icons

---
+++ src/lib/subcomponent/PlatformIcon.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let platform;
+
+	const platforms = {
+		steam: 'steam',
+		xbox: 'xbox',
+		ps4: 'playstation',
+		epic: 'epicgames',
+		psynet: 'nintendoswitch'
+	};
+</script>
+
+<i class={`icon-${platforms[platform]}`} />
+
+<style>
+	i::before {
+		margin-right: 0.4rem;
+		color: var(--color);
+	}
+</style>
