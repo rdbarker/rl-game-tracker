76a3523bd78e9de1a39aa4a8aa85e8e8d4bb342c
rdbarker <ryan.dean.barker@gmail.com>
2022-03-27 15:58:44 -0400

    rank que, rank settings

--- src/lib/scripts/store.js
+++ src/lib/scripts/store.js
@@ -1,4 +1,5 @@
-import { writable, readable } from 'svelte/store';
+import { RankQueue } from '$lib/service/ranksQueue.js';
+import { writable } from 'svelte/store';
 import { MatchesQueue } from '../service/matchesQueue.js';
 import { getItem } from './util.js';
 
@@ -67,9 +68,59 @@ const defaultMatchStats = {
 	}
 };
 
+const defaultPlayerRanks = {
+	ones: {
+		active: true,
+		name: 'Duel',
+		value: 'Ranked Duel 1v1'
+	},
+	doubles: {
+		active: true,
+		name: 'Doubles',
+		value: 'Ranked Doubles 2v2'
+	},
+	threes: {
+		active: true,
+		name: 'Standard',
+		value: 'Ranked Standard 3v3'
+	},
+	unranked: {
+		active: false,
+		name: 'Un-Ranked',
+		value: 'Un-Ranked'
+	},
+	rumble: {
+		active: false,
+		name: 'Rumble',
+		value: 'Rumble'
+	},
+	dropshot: {
+		active: false,
+		name: 'Dropshot',
+		value: 'Dropshot'
+	},
+	hoops: {
+		active: false,
+		name: 'Hoops',
+		value: 'Hoops'
+	},
+	snowday: {
+		active: false,
+		name: 'Snowday',
+		value: 'Snowday'
+	},
+	tournament: {
+		active: true,
+		name: 'Duel',
+		value: 'Ranked Duel 1v1'
+	}
+};
+
 export const apiKey = writable(getItem('bckey'));
 export const isKeyVerified = writable(false);
 export const matchQue = MatchesQueue(60);
-
+export const rankQue = RankQueue(120);
+rankQue.start();
 export const activePlayerStats = persistentStore('playerStats', defaultPlayerStats);
 export const activeMatchStats = persistentStore('matchStats', defaultMatchStats);
+export const activePlayerRanks = persistentStore('playerRanks', defaultPlayerRanks);
