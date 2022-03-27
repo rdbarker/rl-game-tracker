85c605debb0e3d12d52f3307b816357aab0326c3
rdbarker <ryan.dean.barker@gmail.com>
2022-03-27 15:59:24 -0400

    clean up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,6 @@
 <script>
-	import MatchStat from '$lib/subcomponent/MatchStat.svelte';
-	import KeyInput from '$lib/component/KeyInput.svelte';
 	import MatchList from '$lib/component/MatchList.svelte';
 	import Settings from '$lib/component/Settings.svelte';
-	import PlayerCard from '$lib/subcomponent/PlayerCard.svelte';
 </script>
 
 <div>
